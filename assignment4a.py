4a5cbf2ae5958e55f9d88d70631c8e3a97e0bab5
Chinchilla7 <regina.chen@stonybrook.edu>
2022-09-14 23:55:56 -0400

    Update assignment4a.py

--- assignment4a.py
+++ assignment4a.py
@@ -1,3 +1,16 @@
+#load in packages
 import pandas as pd
-import tableone as t1
+from tableone import TableOne, load_dataset
 
+#using sample data to test functions of tableone
+data = pd.read_csv('data.csv')
+list(data)
+data_columns = ['Age', 'HR', 'Smoke', 'Group' ]
+data_categories = ['Smoke', 'Group']
+data_groupby = ['Smoke']
+data_table1 = TableOne(data, columns=data_columns, 
+    categorical=data_categories, groupby=data_groupby, pval=False)
+data_table1 
+print(data_table1.tabulate(tablefmt = "fancy_grid")) #pretty display of data in terminal
+#saving pretty data into new csv
+data_table1.to_csv('/test_data.csv')
